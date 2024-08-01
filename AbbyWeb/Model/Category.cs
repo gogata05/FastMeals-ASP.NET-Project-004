ab71b655f77c5509ee87a2f593c797d95444f424
Georgi Markov <gogata1905@gmail.com>
2024-08-01 17:42:34 +0300

    Data Annotations

--- AbbyWeb/Model/Category.cs
+++ AbbyWeb/Model/Category.cs
@@ -1,7 +1,11 @@
-﻿namespace AbbyWeb.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AbbyWeb.Model;
 public class Category
 {
+    [Key]
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; }
     public int DisplayOrder { get; set; }
 }
