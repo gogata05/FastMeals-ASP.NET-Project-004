bb3ba93a1868b8cc3a7ac39da0a3f8ab3f5f54c8
Georgi Markov <gogata1905@gmail.com>
2024-08-01 17:51:38 +0300

    ApplicationDbContext

---
+++ AbbyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using AbbyWeb.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbbyWeb.Data;
+public class ApplicationDbContext : DbContext
+{
+
+    public DbSet<Category> Category { get; set; }
+}
