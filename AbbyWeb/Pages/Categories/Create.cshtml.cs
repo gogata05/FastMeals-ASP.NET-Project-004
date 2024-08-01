55141592adb9907baaff56a6ecb6933277e33733
Georgi Markov <gogata1905@gmail.com>
2024-08-01 18:59:50 +0300

    Create Property in Page Model

--- AbbyWeb/Pages/Categories/Create.cshtml.cs
+++ AbbyWeb/Pages/Categories/Create.cshtml.cs
@@ -1,9 +1,11 @@
+using AbbyWeb.Model;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AbbyWeb.Pages.Categories;
 
 public class CreateModel : PageModel
 {
+    public Category Category { get; set; }
     public void OnGet()
     {
     }
