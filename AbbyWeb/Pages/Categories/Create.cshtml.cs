21ff9864b43f036b852fa9275d9f70462bc082fc
Georgi Markov <gogata1905@gmail.com>
2024-08-01 19:02:50 +0300

    Create Category - and Bind Property to auto connect data from http requests to .net classes

--- AbbyWeb/Pages/Categories/Create.cshtml.cs
+++ AbbyWeb/Pages/Categories/Create.cshtml.cs
@@ -1,12 +1,30 @@
+using AbbyWeb.Data;
 using AbbyWeb.Model;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
 
 namespace AbbyWeb.Pages.Categories;
 
+[BindProperties]
 public class CreateModel : PageModel
 {
+    private readonly ApplicationDbContext _db;
+
     public Category Category { get; set; }
+
+    public CreateModel(ApplicationDbContext db)
+    {
+        _db = db;
+    }
     public void OnGet()
     {
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        await _db.Category.AddAsync(Category);
+        await _db.SaveChangesAsync();
+        return RedirectToPage("Index");
     }
 }
