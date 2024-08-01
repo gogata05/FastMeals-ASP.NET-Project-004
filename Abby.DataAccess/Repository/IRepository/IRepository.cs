c164ab3e495ce5c988b69bcd6650686b09ec8f33
Georgi Markov <gogata1905@gmail.com>
2024-08-02 01:14:38 +0300

    Create IRepository

---
+++ Abby.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace Abby.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        //GET ALL, GET By ID FIRST OR DEFAULT, ADD, REMOVE, REMOVERANGE
+
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+        IEnumerable<T> GetAll();
+        T GetFirstOrDefault(Expression<Func<T, bool>>? filter = null);
+    }
+}
