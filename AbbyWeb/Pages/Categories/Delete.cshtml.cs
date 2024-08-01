08be64787e38ff523638ea329ba99df554c9e69c
Georgi Markov <gogata1905@gmail.com>
2024-08-01 19:27:00 +0300

    Delete Category

---
+++ AbbyWeb/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,41 @@
+using AbbyWeb.Data;
+using AbbyWeb.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+namespace AbbyWeb.Pages.Categories;
+
+[BindProperties]
+public class DeleteModel : PageModel
+{
+    private readonly ApplicationDbContext _db;
+
+    public Category Category { get; set; }
+
+    public DeleteModel(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    public void OnGet(int id)
+    {
+        Category = _db.Category.Find(id);
+        //Category = _db.Category.FirstOrDefault(u=>u.Id==id);
+        //Category = _db.Category.SingleOrDefault(u=>u.Id==id);
+        //Category = _db.Category.Where(u => u.Id == id).FirstOrDefault();
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        var categoryFromDb = _db.Category.Find(Category.Id);
+        if (categoryFromDb != null)
+        {
+            _db.Category.Remove(categoryFromDb);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+
+        }
+
+        return Page();
+    }
+}
