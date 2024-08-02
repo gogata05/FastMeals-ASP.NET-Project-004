73e805286d586fd3c7f427327ec17f64e8fab6fc
Georgi Markov <gogata1905@gmail.com>
2024-08-02 03:16:18 +0300

    Include Properties

--- Abby.DataAccess/Repository/IRepository/IRepository.cs
+++ Abby.DataAccess/Repository/IRepository/IRepository.cs
@@ -2,14 +2,14 @@
 
 namespace Abby.DataAccess.Repository.IRepository
 {
-    public interface IRepository<T> where T : class
-    {
-        //GET ALL, GET By ID FIRST OR DEFAULT, ADD, REMOVE, REMOVERANGE
+	public interface IRepository<T> where T : class
+	{
+		//GET ALL, GET By ID FIRST OR DEFAULT, ADD, REMOVE, REMOVERANGE
 
-        void Add(T entity);
-        void Remove(T entity);
-        void RemoveRange(IEnumerable<T> entity);
-        IEnumerable<T> GetAll();
-        T GetFirstOrDefault(Expression<Func<T, bool>>? filter = null);
-    }
+		void Add(T entity);
+		void Remove(T entity);
+		void RemoveRange(IEnumerable<T> entity);
+		IEnumerable<T> GetAll(string? includeProperties = null);
+		T GetFirstOrDefault(Expression<Func<T, bool>>? filter = null);
+	}
 }
--- Abby.DataAccess/Repository/Repository.cs
+++ Abby.DataAccess/Repository/Repository.cs
@@ -5,46 +5,58 @@ using System.Linq.Expressions;
 
 namespace Abby.DataAccess.Repository
 {
-    public class Repository<T> : IRepository<T> where T : class
-    {
-        private readonly ApplicationDbContext _db;
-        internal DbSet<T> dbSet;
+	public class Repository<T> : IRepository<T> where T : class
+	{
+		private readonly ApplicationDbContext _db;
+		internal DbSet<T> dbSet;
 
-        public Repository(ApplicationDbContext db)
-        {
-            _db = db;
-            this.dbSet = db.Set<T>();
-        }
+		public Repository(ApplicationDbContext db)
+		{
+			_db = db;
+			//FoodType,Category
+			//_db.MenuItem.Include(u => u.FoodType).Include(u => u.Category);
+			this.dbSet = db.Set<T>();
+		}
 
-        public void Add(T entity)
-        {
-            dbSet.Add(entity);
-        }
+		public void Add(T entity)
+		{
+			dbSet.Add(entity);
+		}
 
-        public IEnumerable<T> GetAll()
-        {
-            IQueryable<T> query = dbSet;
-            return query.ToList();
-        }
+		public IEnumerable<T> GetAll(string? includeProperties = null)
+		{
+			IQueryable<T> query = dbSet;
+			if (includeProperties != null)
+			{
+				//abc,,xyz -> abc xyz
+				foreach (var includeProperty in includeProperties.Split(
+							 new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+				{
+					query = query.Include(includeProperty);
+				}
+			}
 
-        public T GetFirstOrDefault(Expression<Func<T, bool>>? filter = null)
-        {
-            IQueryable<T> query = dbSet;
-            if (filter != null)
-            {
-                query = query.Where(filter);
-            }
-            return query.FirstOrDefault();
-        }
+			return query.ToList();
+		}
 
-        public void Remove(T entity)
-        {
-            dbSet.Remove(entity);
-        }
+		public T GetFirstOrDefault(Expression<Func<T, bool>>? filter = null)
+		{
+			IQueryable<T> query = dbSet;
+			if (filter != null)
+			{
+				query = query.Where(filter);
+			}
+			return query.FirstOrDefault();
+		}
 
-        public void RemoveRange(IEnumerable<T> entity)
-        {
-            dbSet.RemoveRange(entity);
-        }
-    }
+		public void Remove(T entity)
+		{
+			dbSet.Remove(entity);
+		}
+
+		public void RemoveRange(IEnumerable<T> entity)
+		{
+			dbSet.RemoveRange(entity);
+		}
+	}
 }
