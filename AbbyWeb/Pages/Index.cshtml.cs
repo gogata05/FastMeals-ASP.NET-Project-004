b64f5475fc6a9cbd69fce89f647354261816e5fd
Georgi Markov <gogata1905@gmail.com>
2024-08-03 02:01:22 +0300

    Home Page Get Handler

--- AbbyWeb/Pages/Index.cshtml.cs
+++ AbbyWeb/Pages/Index.cshtml.cs
@@ -1,18 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace AbbyWeb.Pages;
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+	private readonly ILogger<IndexModel> _logger;
 
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
+	public IndexModel(ILogger<IndexModel> logger)
+	{
+		_logger = logger;
+	}
 
-    public void OnGet()
-    {
-
-    }
+	public IActionResult OnGet()
+	{
+		return RedirectToPage("Customer/Home/Index");
+	}
 }
