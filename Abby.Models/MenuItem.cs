34ad6d7f81013b5b2895cafed35e733d0e12dd91
Georgi Markov <gogata1905@gmail.com>
2024-08-02 03:04:30 +0300

    Menu Item Upsert Page Get Hander UI

--- Abby.Models/MenuItem.cs
+++ Abby.Models/MenuItem.cs
@@ -4,21 +4,23 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Abby.Models
 {
-    public class MenuItem
-    {
-        [Key]
-        public int Id { get; set; }
-        [Required]
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Image { get; set; }
-        [Range(1, 1000, ErrorMessage = "Price should be between $1 and $1000")]
-        public double Price { get; set; }
-        public int FoodTypeId { get; set; }
-        [ForeignKey("FoodTypeId")]
-        public FoodType FoodType { get; set; }
-        public int CategoryId { get; set; }
-        public Category Category { get; set; }
+	public class MenuItem
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string Image { get; set; }
+		[Range(1, 1000, ErrorMessage = "Price should be between $1 and $1000")]
+		public double Price { get; set; }
+		[Display(Name = "Food Type")]
+		public int FoodTypeId { get; set; }
+		[ForeignKey("FoodTypeId")]
+		public FoodType FoodType { get; set; }
+		[Display(Name = "Category")]
+		public int CategoryId { get; set; }
+		public Category Category { get; set; }
 
-    }
+	}
 }
--- AbbyWeb/Pages/Admin/MenuItems/Upsert.cshtml.cs
+++ AbbyWeb/Pages/Admin/MenuItems/Upsert.cshtml.cs
@@ -2,6 +2,9 @@ using Abby.DataAccess.Repository.IRepository;
 using Abby.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace AbbyWeb.Pages.Admin.MenuItems;
@@ -9,29 +12,41 @@ namespace AbbyWeb.Pages.Admin.MenuItems;
 [BindProperties]
 public class UpsertModel : PageModel
 {
-    private readonly IUnitOfWork _unitOfWork;
+	private readonly IUnitOfWork _unitOfWork;
 
-    public MenuItem MenuItem { get; set; }
+	public MenuItem MenuItem { get; set; }
+	public IEnumerable<SelectListItem> CategoryList { get; set; }
+	public IEnumerable<SelectListItem> FoodTypeList { get; set; }
 
-    public UpsertModel(IUnitOfWork unitOfWork)
-    {
-        _unitOfWork = unitOfWork;
-        MenuItem = new();
-    }
-    public void OnGet()
-    {
-    }
+	public UpsertModel(IUnitOfWork unitOfWork)
+	{
+		_unitOfWork = unitOfWork;
+		MenuItem = new();
+	}
+	public void OnGet()
+	{
+		CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem()
+		{
+			Text = i.Name,
+			Value = i.Id.ToString()
+		});
+		FoodTypeList = _unitOfWork.FoodType.GetAll().Select(i => new SelectListItem()
+		{
+			Text = i.Name,
+			Value = i.Id.ToString()
+		});
+	}
 
-    public async Task<IActionResult> OnPost()
-    {
+	public async Task<IActionResult> OnPost()
+	{
 
-        //if (ModelState.IsValid)
-        //{
-        //    _unitOfWork.FoodType.Add(FoodType);
-        //    _unitOfWork.Save();
-        //    TempData["success"] = "FoodType created successfully";
-        //    return RedirectToPage("Index");
-        //}
-        return Page();
-    }
+		//if (ModelState.IsValid)
+		//{
+		//    _unitOfWork.FoodType.Add(FoodType);
+		//    _unitOfWork.Save();
+		//    TempData["success"] = "FoodType created successfully";
+		//    return RedirectToPage("Index");
+		//}
+		return Page();
+	}
 }
