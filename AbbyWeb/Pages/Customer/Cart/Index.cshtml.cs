3389cd97020fc88cf38cf5e68992c1ccff827ce7
Georgi Markov <gogata1905@gmail.com>
2024-08-03 03:38:37 +0300

    Increment count from shopping cart

--- AbbyWeb/Pages/Customer/Cart/Index.cshtml.cs
+++ AbbyWeb/Pages/Customer/Cart/Index.cshtml.cs
@@ -1,6 +1,7 @@
 using Abby.DataAccess.Repository.IRepository;
 using Abby.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
 using System.Security.Claims;
@@ -32,6 +33,13 @@ namespace AbbyWeb.Pages.Customer.Cart
 					CartTotal += (cartItem.MenuItem.Price * cartItem.Count);
 				}
 			}
+		}
+
+		public IActionResult OnPostPlus(int cartId)
+		{
+			var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+			_unitOfWork.ShoppingCart.IncrementCount(cart, 1);
+			return RedirectToPage("/Customer/Cart/Index");
 		}
 	}
 }
