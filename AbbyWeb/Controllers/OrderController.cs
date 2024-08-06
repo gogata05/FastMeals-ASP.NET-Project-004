77776ef713682f8cb7a36af8fade25014d1dd404
Georgi Markov <gogata1905@gmail.com>
2024-08-06 08:55:41 +0300

    Order List with Datatables

---
+++ AbbyWeb/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using Abby.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AbbyWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult Get()
+        {
+            var OrderHeaderList = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            return Json(new { data = OrderHeaderList });
+        }
+
+
+    }
+}
---
+++ AbbyWeb/Pages/Order/OrderList.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbbyWeb.Pages.Order
+{
+    [Authorize]
+    public class OrderListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
