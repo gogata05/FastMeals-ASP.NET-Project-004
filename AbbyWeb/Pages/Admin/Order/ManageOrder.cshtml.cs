7a5c29b0f579e3518e0e3220f35a22fc212e8ed2
Georgi Markov <gogata1905@gmail.com>
2024-08-08 00:43:04 +0300

    Manage Order Status Update

--- AbbyWeb/Pages/Admin/Order/ManageOrder.cshtml.cs
+++ AbbyWeb/Pages/Admin/Order/ManageOrder.cshtml.cs
@@ -2,12 +2,15 @@ using Abby.DataAccess.Repository.IRepository;
 using Abby.Models;
 using Abby.Models.ViewModel;
 using Abby.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace AbbyWeb.Pages.Admin.Order
 {
+    [Authorize(Roles = $"{SD.ManagerRole},{SD.KitchenRole}")]
     public class ManageOrderModel : PageModel
     {
         private readonly IUnitOfWork _unitOfWork;
@@ -22,7 +25,7 @@ namespace AbbyWeb.Pages.Admin.Order
             OrderDetailVM = new();
 
             List<OrderHeader> orderHeaders = _unitOfWork.OrderHeader.GetAll(u => u.Status == SD.StatusSubmitted ||
-                u.Status == SD.StatusInProcess).ToList();
+            u.Status == SD.StatusInProcess).ToList();
 
             foreach (OrderHeader item in orderHeaders)
             {
@@ -33,6 +36,27 @@ namespace AbbyWeb.Pages.Admin.Order
                 };
                 OrderDetailVM.Add(individual);
             }
+        }
+
+        public IActionResult OnPostOrderInProcess(int orderId)
+        {
+            _unitOfWork.OrderHeader.UpdateStatus(orderId, SD.StatusInProcess);
+            _unitOfWork.Save();
+            return RedirectToPage("ManageOrder");
+        }
+
+        public IActionResult OnPostOrderReady(int orderId)
+        {
+            _unitOfWork.OrderHeader.UpdateStatus(orderId, SD.StatusReady);
+            _unitOfWork.Save();
+            return RedirectToPage("ManageOrder");
+        }
+
+        public IActionResult OnPostOrderCancel(int orderId)
+        {
+            _unitOfWork.OrderHeader.UpdateStatus(orderId, SD.StatusCancelled);
+            _unitOfWork.Save();
+            return RedirectToPage("ManageOrder");
         }
     }
 }
